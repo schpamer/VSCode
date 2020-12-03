7645fb0496b53d7e6c78cf154ef9135b12eeec59
Alex Wellman <schpamer@msn.com>
2020-12-02 19:27:12 -0500

    fron vcsode

---
+++ C++/hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+   cout << "Hello World\n";
+   cout << max(5, 10) << "\n";
+   cout << sqrt(64) << "\n";
+   cout << round(2.6) << "\n";
+   cout << log(2) << "\n";
+   cout << abs(-90) << "\n";
+
+   int x = 5;
+   int y = 15;
+   cout << max(x,y) << "\n";
+   bool isCodingFun = true;
+   bool isFishTasty = false;
+   cout << isCodingFun << "\n";
+   cout << isFishTasty;
+   return 0;
+}
